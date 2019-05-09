39066290a51b96a610805c0e9329c8c37ea99232
Tran Danh Tuan <end2end8x@gmail.com>
2019-05-09 14:24:07 +0700

    #CK-006 update integration spec

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -3,18 +3,18 @@ require_relative '../../lib/checkout'
 describe 'Integration Specs' do
   let(:products_total_price_over) do
     {
-        001 => Product.new('Lavender heart', 9.25),
-        002 => Product.new('Personalised cufflinks', 45.0),
-        003 => Product.new('Kids T-shirt', 19.95)
+        001 => Product.new('001', 'Lavender heart', 9.25),
+        002 => Product.new('002', 'Personalised cufflinks', 45.0),
+        003 => Product.new('003', 'Kids T-shirt', 19.95)
     }
   end
   subject(:checkout_total) { Checkout.new products: products_total_price_over }
 
   let(:products_buy_more_lavender) do
     {
-        001 => Product.new('Lavender heart', 9.25),
-        003 => Product.new('Kids T-shirt', 19.95),
-        001 => Product.new('Lavender heart', 9.25)
+        001 => Product.new('001', 'Lavender heart', 9.25),
+        003 => Product.new('003', 'Kids T-shirt', 19.95),
+        001 => Product.new('001', 'Lavender heart', 9.25)
     }
   end
   subject(:checkout_more_lavender) { Checkout.new products: products_buy_more_lavender }
